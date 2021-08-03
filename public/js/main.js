d6da7f3470be9972d28877dd10e7d3cb0c840c6d
iyurik1996i <iyurik1996i@gmail.com>
2021-08-03 19:56:03 +0300

    half home page

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+    $(".response__slider").slick({
+        dots: false,
+        vertical: true,
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
