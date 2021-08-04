acd94ac8b1524c90d425c0acd18dda3177f7eee8
iyurik1996i <iyurik1996i@gmail.com>
2021-08-05 00:51:16 +0300

    home page without adaptive

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
-$(document).on('ready', function() {
+$(document).ready (function() {
     $('.response__slider').slick({
         dots: false,
-        vertical: true,
-        centerMode: true,
         slidesToShow: 1,
         slidesToScroll: 1
     });
