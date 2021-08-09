b7a220ab374dc414617e5a65d667ae0537e8cc38
iyurik1996i <iyurik1996i@gmail.com>
2021-08-09 16:48:34 +0300

    some adaptive

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,15 @@ $(document).ready (function() {
         slidesToShow: 1,
         slidesToScroll: 1
     });
+
     $('.reply').slideToggle();
     $('.question').click(function() {
         $(this).toggleClass('active');
         $(this).parent().children('.reply').slideToggle(300);
+    });
+
+    $('.menu__icon').click(function() {
+        $(this).toggleClass('close')
+        $(this).parent().children('.menu__body').slideToggle(300);
     });
 });
