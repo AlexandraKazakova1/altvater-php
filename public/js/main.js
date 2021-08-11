d17d1a647df3a8755a0983b44e40a8a344c9f27f
iyurik1996i <iyurik1996i@gmail.com>
2021-08-11 20:02:33 +0300

    nav fix

--- public/js/main.js
+++ public/js/main.js
@@ -108,3 +108,10 @@ $(document).ready (function() {
         }
     });
 });
+
+$(document).ready (function() {
+    $(".scroll").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
