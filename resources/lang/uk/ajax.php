194a84d28ca20e4d588bd7afee2672e2dce44df5
mirasoft <mirasoft@mail.ru>
2021-08-20 04:59:48 +0300

    SendController

---
+++ resources/lang/uk/ajax.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'success_send_feedback'         	=> 'Повідомлення успішно відправлено',
+	'failed_send_feedback'          	=> 'Не вдалося відправити повідомлення',
+	
+	'method_not_found'                  => 'Метод не знайдено',
+	'product_not_found'                 => 'Продукт не знайдено',
+	
+	'data_save_success'                 => 'Дані успішно збережено',
+	'data_save_failed'                  => 'Неможливо зберегти дані',
+	
+	'you_need_to_log_in'                => 'Необхідно авторизуватись',
+	
+	'field_required'					=> 'Заповніть обов\'язкові дані'
+];
