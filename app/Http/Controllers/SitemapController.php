34b6dda58a2be3a07043de54eaa3fcf02c5e436b
mirasoft <mirasoft@mail.ru>
2021-08-31 14:35:10 +0300

    Карта сайту

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Pages;
 use App\Models\News;
 use App\Models\Services;
-use App\Models\FAQ;
-use App\Models\Reviews;
 
 use App\Helpers\MyBreadcrumbs;
 use App\Helpers\StringHelper;
@@ -28,19 +26,52 @@ class SitemapController extends MyController {
 	public function index(){
 		$page = (object)Pages::query()->where('slug', 'sitemap.html')->first()->toArray();
 		
+		$pages = [];
+		
+		$tmp = Pages::query()->orderBy('title', 'asc')->get()->toArray();
+		
+		foreach($tmp as $item){
+			$item = (object)$item;
+			
+			if($item->slug == 'index'){
+				$item->slug = '/';
+			}
+			
+			$item->pages = [];
+			
+			$pages[$item->pages] = $item;
+		}
+		
+		$services = Services::query()->where('public', 1)->orderBy('created_at', 'desc')->select('slug', 'title')->get()->toArray();
+		
+		foreach($services as $item){
+			$item = (object)$item;
+			
+			$pages[9]->pages[] = $item;
+		}
+		
+		$news = News::query()->where('public', 1)->orderBy('created_at', 'desc')->select('slug', 'title')->get()->toArray();
+		
+		foreach($news as $item){
+			$item = (object)$item;
+			
+			$pages[7]->pages[] = $item;
+		}
+		
 		$data = [
-			'page'		=> array(
+			'page'			=> array(
 				'title'			=> $page->title,
 				'keywords'		=> $page->keywords,
 				'description'	=> $page->description,
 				'uri'			=> 'sitemap.html',
 				'og_image'		=> '',
 			),
-			'robots'	=> $page->robots,
-			'canonical'	=> $page->canonical,
-			'data'		=> $page,
-			'services'	=> Services::query()->where('public', 1)->orderBy('created_at', 'desc')->select('slug', 'title', 'image')->get(),
-			'news'		=> News::getNew()
+			'robots'		=> $page->robots,
+			'canonical'		=> $page->canonical,
+			'data'			=> $page,
+			'all_pages'		=> $pages
+			//'services'	=> Services::query()->where('public', 1)->orderBy('created_at', 'desc')->select('slug', 'title', 'image')->get(),
+			//'news'		=> News::getNew()
 		];
 		
 		return view(
--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -5,7 +5,32 @@
 		<section class="detail__wrapper">
 			<div class="detail container">
 				<div class="detail__text">
-					{!!$data->text!!}
+					<div class="sitemap-container">
+						<ul>
+						@foreach($all_pages as $item)
+							<li>
+							@if($item->public)
+								<a class="hover-color" href="{{url($item->slug)}}">{{$item->name}}</a>
+							@else
+								<a class="hover-color" href="" onclick="return false;">{{$item->name}}</a>
+							@endif
+							@if(isset($item->pages) && $item->pages)
+								<ul>
+								@foreach($item->pages as $sub)
+									<li>
+									@if($sub->public)
+										<a href="{{url($item->slug.'/'.$sub->slug)}}">{{$sub->name}}</a>
+									@else
+										<a href="" onclick="return false;">{{$sub->name}}</a>
+									@endif
+									</li>
+								@endforeach
+								</ul>
+							@endif
+							</li>
+						@endforeach
+						</ul>
+					</div>
 				</div>
 			</div>
 		</section>
