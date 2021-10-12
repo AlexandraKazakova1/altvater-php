6fda64335ed5b1dfd686edc0e5f972ee2870ef9e
mirasoft <mirasoft@mail.ru>
2021-10-12 20:21:08 +0300

    servicesCalc

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,6 +26,7 @@
 							<!-- -->
 						@endforeach
 					</div>
+					<button class="calc-btn btn-red" data-toggle="modal" data-target="#servicesCalc">Розрахунок вартості наших послуг</button>
 				</div>
 			</section>
 		@endif
@@ -179,4 +180,105 @@
 			</section>
 		@endif
 	</main>
+	
+	<div class="modal fade" id="servicesCalc" tabindex="-1" role="dialog">
+		<div class="modal-dialog modal-md modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="servicesCalc-page popup">
+					<div class="modal-header">
+						<h2 class="popup__title">Для розрахунку заповніть поля</h2>
+						
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<img src="/img/close.svg" alt="X">
+						</button>
+					</div>
+					
+					<div class="modal-body">
+						<form class="popup__form" id="servicesCalc-form">
+							<fieldset class="fieldset">
+								<Label class="select">
+									<span class="input-description">Об'єкт утворення ТПВ</span>
+									<select class="select2" name="type">
+										<option name="apartments">Багатоквартирні будинки</option>
+										<option name="private">Приватний будинок</option>
+										<option name="office">Офіс</option>
+										<option name="industrial">Промислове підприємство</option>
+									</select>
+								</label>
+								
+								<Label>
+									<span class="input-description">Кількість людей</span>
+									<input class="form-control" type="text" name="count" autocomplete="off" oninput="this.value=this.value.replace(/[^0-9]/g,'');" placeholder=" "/>
+								</Label>
+								
+								<Label class="select">
+									<span class="input-description">Категорія тарифу</span>
+									<select class="select2" name="category">
+										<option value="people">Населення</option>
+										<option value="commerce">Комерція</option>
+									</select>
+								</Label>
+								
+								<Label class="select">
+									<span class="input-description">Об'єм контейнера, м<sup>3</sup></span>
+									<select class="select2" name="volume">
+										<option value="1.1">1,1</option>
+										<option value="2.2">2,2</option>
+										<option value="4.1">4,1</option>
+									</select>
+								</Label>
+								
+								<Label class="select">
+									<span class="input-description">Бажана періодичність вивезень, разів на тиждень</span>
+									<select class="select2" name="period">
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+										<option value="6">6</option>
+										<option value="7">7</option>
+									</select>
+								</Label>
+								
+								<Label class="readonly">
+									<span class="input-description">Норма, м<sup>3</sup>/рік на одиницю розрахунку</span>
+									<input class="form-control" type="text" readonly />
+								</Label>
+								
+								<Label class="readonly">
+									<span class="input-description">&#8776; тонн/рік (для декларації, від 50т)</span>
+									<input class="form-control" type="text" readonly />
+								</Label>
+								
+								<Label class="readonly">
+									<span class="input-description">Тариф, грн/1 м<sup>3</sup> з ПДВ</span>
+									<input class="form-control" type="text" readonly />
+								</Label>
+								
+								<Label class="readonly">
+									<span class="input-description">Рекомендована мінімальна кількість вивезень на тиждень &#8776;</span>
+									<input class="form-control" type="text" readonly style="font-weight: bold;" />
+								</Label>
+								
+								<Label class="readonly">
+									<span class="input-description">*Вартість послуг за місяць, грн. з ПДВ &#8776;</span>
+									<input class="form-control" type="text" readonly style="font-weight: bold;" />
+								</Label>
+								
+								<Label class="readonly">
+									<span class="input-description">**Потреба в контейнерах, шт.</span>
+									<input class="form-control" type="text" readonly style="font-weight: bold;" />
+								</Label>
+								
+								<p>*Остаточну вартість та умови визначає менеджер при оформленні!</p>
+								
+								<a class="btn-red" href="/contacts">Оформити заявку</a>
+							</fieldset>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @stop
