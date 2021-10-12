3a004c207af91fe01a0c869477d7609b204990d5
Юра Спринчук <iyurik1996i@gmail.com>
2021-10-12 15:37:45 +0300

    cucicookie fix

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,7 @@ $(document).ready (function() {
     passRecovery();
     passRecovery3();
     passVerificationForm();
+    checkCookies();
 });
 
 function slidersConfig() {
@@ -606,3 +607,20 @@ function passVerificationForm() {
 	    }
     });
 };
+
+function checkCookies(){
+    let cookieDate = localStorage.getItem('cookieDate');
+    let cookieNotification = document.getElementById('cookie_notification');
+    let cookieBtn = cookieNotification.querySelector('.cookie_accept');
+
+    // Если записи про кукисы нет или она просрочена на 1 год, то показываем информацию про кукисы
+    if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
+        cookieNotification.classList.add('show');
+    }
+
+    // При клике на кнопку, в локальное хранилище записывается текущая дата в системе UNIX
+    cookieBtn.addEventListener('click', function(){
+        localStorage.setItem( 'cookieDate', Date.now() );
+        cookieNotification.classList.remove('show');
+    })
+}
