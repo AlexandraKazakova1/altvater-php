4a1122c450a3cbdf1efc113147a8892c8d263bc4
mirasoft <mirasoft@mail.ru>
2021-10-20 01:24:42 +0300

    header btn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -94,7 +94,7 @@
 				<div class="header-nav__btn-group">
 					@if($settings['header_btn'])
 						@if($user)
-							<a href="/account" class="btn-logIn">Мій кабінет</a>
+							<a href="/account" class="btn-header">Мій кабінет</a>
 						@else
 							<button type="button" class="btn-logIn" data-toggle="modal" data-target="#log__in-modal">Вхід</button>
 							<button type="button" class="btn-reg btn-red" data-toggle="modal" data-target="#create-modal">Реєстрація</button>
@@ -121,7 +121,7 @@
 							<div class="menu-nav__btn-group">
 								@if($settings['header_btn'])
 									@if($user)
-										<a href="/account" class="btn-logIn">Мій кабінет</a>
+										<a href="/account" class="btn-header">Мій кабінет</a>
 									@else
 										<a href="/login" class="btn-logIn">Вхід</a>
 										<a href="/registration" class="btn-reg btn-red">Реєстрація</a>
