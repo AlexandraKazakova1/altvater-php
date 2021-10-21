619f35ebe03be3fb20ba8f1963c88bddbf876a82
Юра Спринчук <iyurik1996i@gmail.com>
2021-10-21 03:42:18 +0300

    fix and msg page

--- public/js/cabinet.js
+++ public/js/cabinet.js
@@ -15,6 +15,7 @@ $(document).ready (function() {
     contractEntity();
     addAddress();
     requestForm();
+    // pdfViever();
 
     $('.add').click(function() {
         $('.modal').modal('hide');
@@ -1132,3 +1133,29 @@ function settingsForm() {
     });
 };
 
+// function pdfViever() {
+//     PDFJS.getDocument('helloworld.pdf').then(function(pdf) {
+//         // Using promise to fetch the page
+//         pdf.getPage(1).then(function(page) {
+//             var scale = 1.5;
+//             var viewport = page.getViewport(scale);
+        
+//             //
+//             // Prepare canvas using PDF page dimensions
+//             //
+//             var canvas = document.getElementById('the-canvas');
+//             var context = canvas.getContext('2d');
+//             canvas.height = viewport.height;
+//             canvas.width = viewport.width;
+        
+//             //
+//             // Render PDF page into canvas context
+//             //
+//             var renderContext = {
+//                 canvasContext: context,
+//                 viewport: viewport
+//             };
+//             page.render(renderContext);
+//         });
+//     });
+// }
