0c5874991edc4c11c9928ad9e2afb601a9936de8
mirasoft <mirasoft@mail.ru>
2021-10-25 06:19:39 +0300

    dialog

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -156,7 +156,7 @@ class AccountController extends MyController {
 		
 		Messages::query()->where('id', $id)->update(['read' => 1]);
 		
-		$messages = Messages::query()->where('client_id', $this->_id)->where('dialogue_id', $id)->orderBy('created_at', 'desc')->get();
+		$messages = Messages::query()->where('dialogue_id', $id)->orderBy('created_at', 'desc')->get();
 		
 		return view(
 			'account/dialog',
