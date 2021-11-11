21609a838fc3f3b5505aa4413642bbf8414242db
mirasoft <mirasoft@mail.ru>
2021-11-11 13:51:45 +0200

    contracts

--- app/Http/Controllers/Ajax/ContractsController.php
+++ app/Http/Controllers/Ajax/ContractsController.php
@@ -282,7 +282,7 @@ class ContractsController extends Controller {
 			
 			$payload['count'] = Contracts::query()->where('client_id', $this->_id)->whereRaw(DB::raw('(`archive` IS NULL OR `archive` = 0)'))->count();
 			
-			$offset = (int)$this->get('offset');
+			$offset = (int)$request->get('offset');
 			$limit	= 4;
 			
 			if($offset < 0){
