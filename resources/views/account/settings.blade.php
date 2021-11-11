d7dd113cc06fcd0198da9613c8207f7f8d7f32b8
mirasoft <mirasoft@mail.ru>
2021-11-11 19:03:21 +0200

    company info

--- resources/views/account/settings.blade.php
+++ resources/views/account/settings.blade.php
@@ -97,7 +97,7 @@
 							</span>
 						</label>
 						
-						@if($user['company_name'] == 'legal-entity')
+						@if($user['type'] == 'legal-entity')
 							<label>
 								<span class="input-description notCheck">Назва компанії:<b> ?</b></span>
 								<input class="form-control" type="text" name="company_name" readonly value="{{$user['company_name']}}" placeholder=" ">
