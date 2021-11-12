13aec704264dc85340781a3999d6b26179313ef8
mirasoft <mirasoft@mail.ru>
2021-11-12 14:03:20 +0200

    number

--- resources/views/account/layout.blade.php
+++ resources/views/account/layout.blade.php
@@ -381,7 +381,7 @@
 									
 									<label>
 										<span class="input-description">Номер договору:</span>
-										<input class="form-control" type="text" name="number" placeholder=" " autocomplete="off" oninput="this.value=this.value.replace(/[^0-9]/g,'');" placeholder=" ">
+										<input class="form-control" type="text" name="number" placeholder=" " autocomplete="off" placeholder=" ">
 										<span class="input-placeholder">
 											<div>
 												<img src="/img/cabinet/documents.svg">
