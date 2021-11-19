f70027f01ca33e86d6fb3a57198baf4fb954a412
mirasoft <mirasoft@mail.ru>
2021-11-19 16:06:36 +0200

    orderService__wrapper

--- resources/views/components/service.blade.php
+++ resources/views/components/service.blade.php
@@ -1,8 +1,7 @@
 <section class="orderService__wrapper">
-	<form class="orderService__form conteiner">
-		<h2 class="popup__title">Замовити послугу</h2
-		
-		<fieldset>
+	<form class="orderService__form">
+		<h2 class="popup__title">Замовити послугу</h2>
+		<fieldset class="callback-fieldset">
 			<label>
 				<span class="input-description">Оберіть послугу:</span>
 				<select class="custom-select2" name="service">
@@ -10,24 +9,19 @@
 					<option value="Послуга-1">Послуга-1</option>
 				</select>
 			</label>
-			
-			<label>
-				<span>Ім'я </span>
-				<input class="form-control" type="text" id="username" name="name" placeholder="Ваше Ім'я">
-			</label>
-			
 			<label>
 				<span>Телефон</span>
 				<input class="form-control" type="tel" id="userphone" name="phone" placeholder="Ваш телефон">
 			</label>
-			
+			<label>
+				<span>Ім'я </span>
+				<input class="form-control" type="text" id="username" name="name" placeholder="ПІБ">
+			</label>
 			<label>
 				<span>Повідомлення</span>
-				<textarea placeholder="Ваше Повідомлення" id="massage" name="massage"></textarea>
+				<textarea placeholder="Ваше повідомлення" id="massage" name="massage"></textarea>
 			</label>
-			
 			<button class="btn-submit" type="submit" name="submit">Залишити заявку</button>
-			
 			<label class="checkbox__label">
 				<input class="input-checkbox" type="checkbox" id="rule" name="rule">
 				<span class="custom-checkbox"></span>
