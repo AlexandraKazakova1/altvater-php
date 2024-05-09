e857c7f9fd9b3b22733a5c075fb42352300978d6
Viktor O. Lamaiev <info@zanshee.kiev.ua>
2024-05-09 17:50:24 +0300

    AV-4: Add gtag in layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,15 @@
 		})(window,document,'script','dataLayer','GTM-MH9GJK6V');</script>
 		<!-- End Google Tag Manager -->
 
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=AW-673981964"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+		  gtag('config', 'AW-673981964');
+		</script>
+
 		<meta charset="utf-8">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
